b566a1209a2ccea36826314cb9d5707e41a4eb21
Legus-Yeung <legus.yeung@gmail.com>
2025-09-27 11:29:33 -0400

    t3 initial commit, add postgres url next

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+/** @type {import("next").NextConfig} */
+const config = {
+  turbopack: {
+    root: resolve(__dirname),
+  },
+};
+
+export default config;
